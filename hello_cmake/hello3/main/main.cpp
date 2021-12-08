1b12d3889c860a5b3b09ade73178f408f320764b
Richard <richard_ece2k5@yahoo.com>
2021-12-09 00:14:35 +0800

    Add support for external package

--- hello_cmake/hello3/main/main.cpp
+++ hello_cmake/hello3/main/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "operations.hpp"
+#include <boost/random.hpp>
 
 int main() {
 	std::cout<<"Hello CMake!"<<std::endl;
         math::operations op;
         int sum = op.sum(3, 4);
 	std::cout<<"Sum of 3 + 4 :"<<sum<<std::endl;
+
+    //Add Boost support
+    boost::mt19937 rng;
+    double mean = 2.3;
+    double std = 0.34;
+    auto normal_dist = boost::random::normal_distribution<double>(mean, std);
+    boost::variate_generator<boost::mt19937&,
+            boost::normal_distribution<> > random_generator(rng, normal_dist);
+    
+    for(int i = 0; i < 2; i++){
+        auto rand_val = random_generator();
+        std::cout<<"Random Val "<<i+1<<" :"<<rand_val<<std::endl;
+    }
+
 	return 0;
 }
